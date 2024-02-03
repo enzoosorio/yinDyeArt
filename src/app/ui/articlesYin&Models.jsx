40313e1afab727475383d2a1a3da30bbedcbfba5
enzo O <enzoosorio2010@hotmail.com>
2024-02-03 16:48:50 -0500

    adding the images of the models and fixing some irregular margins

--- src/app/ui/articlesYin&Models.jsx
+++ src/app/ui/articlesYin&Models.jsx
@@ -1,34 +1,161 @@
+"use client";
+
 import Image from "next/image";
 import styles from "../styles/articles.module.css";
 import LeftArrow from "../../svgs/arrow-left.svg";
 import RightArrow from "../../svgs/arrow-right.svg";
 import { robotoSlabFont, ebGaramondFont } from "../lib/fonts";
+import Yin1 from "../../../public/yin/yinyindye1.jpeg";
+import Yin2 from "../../../public/yin/yinyindye2.jpeg";
+import Yin3 from "../../../public/yin/yinyindye3.jpeg";
+import Yin4 from "../../../public/yin/yinyindye4.jpeg";
+import Yin5 from "../../../public/yin/yinyindye5.jpeg";
+import Yin6 from "../../../public/yin/yinyindye6.jpeg";
+import Yin7 from "../../../public/yin/yinyindye7.jpeg";
+import { Models } from "./models";
+import { useEffect, useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 export const ArticlesWrapper = () => {
+  const cat = [
+    { image: Yin1, about: "cat sleeping in the sofa" },
+    { image: Yin2, about: "cat biting my foot" },
+    { image: Yin3, about: "cat on the wood chair with a lunch bag" },
+    { image: Yin4, about: "cat sleeping in the sofa again" },
+    { image: Yin5, about: "cat seeing the camera before he sleep in the sofa" },
+    { image: Yin6, about: "cat in the christmas tree box" },
+    { image: Yin7, about: "tumblr-cat foto" },
+  ];
+
+  const [imageCounter, setImageCounter] = useState(1);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleLeftClick = (e) => {
+    if (imageCounter > 1) {
+      setImageCounter(imageCounter - 1);
+    }
+    console.log(imageCounter);
+  };
+
+  const handleRightClick = (e) => {
+    if (imageCounter < cat.length) {
+      setImageCounter(imageCounter + 1);
+    }
+    console.log(imageCounter);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Actualiza el estado cuando cambia el tamaño de la ventana
+    };
+    if (typeof window !== "undefined") {
+      handleResize(); // Llama a la función inicialmente para establecer el estado
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
+  //me ayuda a que se conserve el index al momento de pasar de mobile a desktop (por cualquier cosa)
+  //no funciona si se cambia el index de desktop y se quiere pasar a mobile ):
+  const handleSlideChange = (swiper) => {
+    setImageCounter(swiper.realIndex + 1);
+  };
+
   return (
     <section className={styles.articlesWrapper}>
       <article className={styles.articleOne}>
-        <div className={styles.imagesYin}>
-          <Image src={LeftArrow} width={41} height={42} alt="left arrow" />
-          {/* falta la coleccion de imagenes */}
-          <Image src={RightArrow} width={42} height={42} alt="right arrow" />
+        <div className={styles.imagesYinwithButtons}>
+          {isMobile ? (
+            <Swiper
+              navigation={{
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              }}
+              pagination={{
+                dynamicBullets: true,
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              onSlideChange={handleSlideChange}
+              className={`mySwiper ${styles.wrapperYinImagesSlider}`}
+            >
+              {cat.map((item, index) => (
+                <SwiperSlide key={index}>
+                  <Image
+                    src={item.image}
+                    width={240}
+                    height={320}
+                    alt={item.about}
+                    className={styles.imageYin}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <>
+              <button onClick={handleLeftClick} disabled={imageCounter === 1}>
+                <IoIosArrowBack
+                  className={`${styles.arrowBack} ${
+                    imageCounter === 1 ? styles.arrowBackDisabled : ``
+                  }`}
+                />
+              </button>
+              <div className={styles.wrapperYinImages}>
+                <ul
+                  className={`${styles.ulWrapperYinImages} ${
+                    imageCounter === 2
+                      ? styles.showImageTwo
+                      : imageCounter === 3
+                      ? styles.showImageThree
+                      : imageCounter === 4
+                      ? styles.showImageFour
+                      : imageCounter === 5
+                      ? styles.showImageFive
+                      : imageCounter === 6
+                      ? styles.showImageSix
+                      : imageCounter === 7
+                      ? styles.showImageSeven
+                      : ""
+                  }`}
+                >
+                  {cat.map((item, index) => (
+                    <li key={index} className={styles.item__imageYin}>
+                      <Image
+                        src={item.image}
+                        width={320}
+                        height={426.67}
+                        alt={item.about}
+                        className={styles.imageYin}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <button onClick={handleRightClick} disabled={imageCounter === 7}>
+                <IoIosArrowForward
+                  className={`${styles.arrowForward} ${
+                    imageCounter === 7 ? styles.arrowForwardDisabled : ``
+                  }`}
+                />
+              </button>
+            </>
+          )}
         </div>
         <h3 className={robotoSlabFont.className}>
-          El nombre de la página nace a partir de nuesto gato Yin, quien
-          obviamente ha inspirado para ser el nombre de la página. Compartimos
-          fotos de él con ustedes. 😸
+          Yin es nuestro gatito, el preferido de la familia . Nació con el don
+          de ser modelo, así que compartimos fotos de él con ustedes. 😸
         </h3>
       </article>
-      <article className={styles.articleTwo}>
-        <p>Lorem ipsum dolor sit </p>
-        <div
-          className={`${styles.messageModelsWrapper} ${ebGaramondFont.className}`}
-        >
-          <h3>Nuestros Modelos</h3>
-          <span>❤</span>
-        </div>
-        <p>Lorem ipsum dolor sit</p>
-      </article>
+      <Models />
     </section>
   );
 };
---
+++ src/app/ui/models.jsx
@@ -0,0 +1,172 @@
+import Image from "next/image";
+import styles from "../styles/articles.module.css";
+import { ebGaramondFont } from "../lib/fonts";
+import Marly1 from "../../../public/models/marly/marly2.JPG";
+import Marly2 from "../../../public/models/marly/marly3.JPG";
+import Marly3 from "../../../public/models/marly/marly4.JPG";
+import Marly4 from "../../../public/models/marly/marly5.JPG";
+import Marly5 from "../../../public/models/marly/marly6.JPG";
+import Marly6 from "../../../public/models/marly/marly8.JPG";
+import Marly7 from "../../../public/models/marly/marly9.JPG";
+import Marly8 from "../../../public/models/marly/marly10.JPG";
+//----
+import FarroSofia1 from "../../../public/models/sofia_farro/farro_sofia1.jpg";
+import FarroSofia2 from "../../../public/models/sofia_farro/farro_sofia3.jpg";
+import FarroSofia3 from "../../../public/models/sofia_farro/farro_sofia4.jpg";
+import FarroSofia4 from "../../../public/models/sofia_farro/farro_sofia5.jpg";
+import FarroSofia5 from "../../../public/models/sofia_farro/farro_sofia7.jpg";
+import FarroSofia6 from "../../../public/models/sofia_farro/farro_sofia8.jpg";
+import FarroSofia7 from "../../../public/models/sofia_farro/farro_sofia9.jpg";
+import FarroSofia8 from "../../../public/models/sofia_farro/farro_sofia10.jpg";
+import FarroSofia9 from "../../../public/models/sofia_farro/farro_sofia11.jpg";
+import { useEffect, useState } from "react";
+
+export const Models = () => {
+  const modelLeft = [
+    {
+      image: FarroSofia1,
+      width: "323.22",
+      height: "404",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia2,
+      width: "323.71",
+      height: "404.57",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia3,
+      width: "322.87",
+      height: "403.6",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia4,
+      width: "323.94",
+      height: "404.89",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia5,
+      width: "323.39",
+      height: "404.18",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia6,
+      width: "323.22",
+      height: "404",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia7,
+      width: "323.22",
+      height: "404",
+      alt: "photo-gallery in the photoStudio",
+    },
+    {
+      image: FarroSofia8,
+      width: "323.22",
+      height: "404",
+      alt: "photo-gallery in the photoStudio",
+    },
+    // {
+    //   image: FarroSofia9,
+    //   width: "323.22",
+    //   height: "404",
+    //   alt: "photo-gallery in the photoStudio",
+    // },
+  ];
+
+  const modelRight = [
+    {
+      image: Marly1,
+      width: "323.22",
+      height: "404",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly2,
+      width: "323.71",
+      height: "404.57",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly3,
+      width: "322.87",
+      height: "403.6",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly4,
+      width: "323.94",
+      height: "404.89",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly5,
+      width: "323.39",
+      height: "404.18",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly6,
+      width: "323.22",
+      height: "404",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly7,
+      width: "323.22",
+      height: "404",
+      alt: "photo in the beach",
+    },
+    {
+      image: Marly8,
+      width: "323.22",
+      height: "404",
+      alt: "photo in the beach",
+    },
+  ];
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % modelLeft.length);
+    }, 800);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentImage, modelLeft.length]);
+
+  return (
+    <article className={styles.articleTwo}>
+      <div className={styles.wrapperModelLeft}>
+        <Image
+          src={modelLeft[currentImage].image}
+          width={320}
+          height={401}
+          alt={modelLeft[currentImage].alt}
+          className={styles.modelImage}
+        />
+      </div>
+      <div
+        className={`${styles.messageModelsWrapper} ${ebGaramondFont.className}`}
+      >
+        <h3>Nuestros Modelos</h3>
+        <span>❤</span>
+      </div>
+      <div className={styles.wrapperModelRight}>
+        <Image
+          src={modelRight[currentImage].image}
+          width={320}
+          height={401}
+          alt={modelRight[currentImage].alt}
+          className={styles.modelImage}
+        />
+      </div>
+    </article>
+  );
+};
